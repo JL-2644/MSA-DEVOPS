c44f004b1780489a179dee4c7e91df5d50cc9cd4
Jia Qi <04jiaqi@gmail.com>
2020-07-25 08:36:16 +1200

    update website

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './globe.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="https://images.pexels.com/photos/45208/world-earth-planet-globe-45208.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" className="App-logo" alt="logo" />
+        <img src="https://www.openmoji.org/data/color/svg/1F30F.svg" className="App-logo" alt="logo" />
         <p>
           Earth
         </p>
         <a
           className="App-link"
-          href="https://en.wikipedia.org/wiki/Earth"
+          href="https://en.wikipedia.org/wiki/Oceania"
           target="_blank"
           rel="noopener noreferrer"
         >
